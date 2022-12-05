f5a1233f8f41f7ef158e87d6268b6b1e5603e2c9
yego <yegokipkemoi254@gmail.com>
2022-12-05 06:19:35 +0300

    Done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250}
+    validates :summary, length: { maximum: 250}
+    validates :category, inclusion: {in: ['Fiction', 'Non-Fiction'] } 
+
+    validate :is_clickbait?
+
+    def is_clickbait?
+		valids = [/Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i]
+		if valids.none?{|par| par.match title}
+			errors.add(:title, "not clickbait")
+		end
+	end
 end
